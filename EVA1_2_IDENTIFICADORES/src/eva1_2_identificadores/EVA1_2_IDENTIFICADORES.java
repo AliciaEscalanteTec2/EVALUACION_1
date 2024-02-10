3ff74e85a54e89a17082163c21b214a4287780cd
AliciaEscalanteTec2 <159506688+AliciaEscalanteTec2@users.noreply.github.com>
2024-02-09 17:16:31 -0700

    Add files via upload

---
+++ EVA1_2_IDENTIFICADORES/src/eva1_2_identificadores/EVA1_2_IDENTIFICADORES.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_identificadores;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_2_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //VARIABLES: 
+        //TIPO DE DATO + NOMBRE DEL IDENTIFICADOR:
+        //short int byte long --> Enteros
+        //float double --> Números con decimal
+        //char --> character --> Caracteres (solo una letra)
+        //boolean --> Verdadera o falso
+        //String --> Cadenas de texto 
+        
+        //DECLARACIÓN DE UNA VARIABLE:
+        //Si no se declara una variable, no se puede usar
+        int edad; //Declaración de una edad
+        String nombre; //Declaración del nombre
+        double salario; //Declaración del salario en decimal
+        
+        //ASIGNAIÓN DE VALORES;
+        //IDENTIFICADOR = VALOR;
+        //Se coloca el valor 50 dentro de la variable edad
+        edad = 50;
+        nombre = "Alicia Escalante";
+        //Si en el texto no le pongo con comillas dobles, lo va a tomar
+        //como identificador
+        salario = 123.56; 
+        
+        //COMO LOS USAMOS:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);  
+        
+    }
+    
+}
+//Alicia Escalante Sosa
+//Numero de control: 24550008
+//Grupo: B
