65817bf8259c171460aa42381c98b37345f9f996
AliciaEscalanteTec2 <159506688+AliciaEscalanteTec2@users.noreply.github.com>
2024-02-15 21:48:11 -0700

    Add files via upload

---
+++ EVA1_4_ASIGNACIÓN/src/eva1_4_asignación/EVA1_4_ASIGNACIÓN.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_asignación;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_4_ASIGNACIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Declaración de la variable: 
+        int edad;
+        
+        //Darle una asignación 
+        edad = 22; //Un solo = significa asignar 
+        
+        System.out.println("Tu edad es: ");
+        System.out.println(edad);
+        
+        System.out.println("------------------------");
+        
+        String nombre; 
+        String carrera; 
+        
+        nombre = "Alicia Escalante Sosa";
+        carrera = "Ingenieria en sistemas computacionales";
+        
+        System.out.println("Mi nombre es: "+nombre);
+        System.out.println("Mi carrera es: "+carrera);
+        System.out.println("Gracias :)");
+        
+        //ALICIA ESCALANTE SOSA  NO.CONTROL: 24550008
+    }
+    
+}
